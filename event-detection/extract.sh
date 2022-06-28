20aece45afd14dce391df494d0185da85f2eef33
elanhermi <hlne.herman@gmail.com>
2022-06-28 18:26:28 +0200

    Anyone anywhere shell scripts

--- event-detection/extract.sh
+++ event-detection/extract.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-minet extract -i FETCHED FETCHED.CSV -o EXTRACTED.CSV
+
+INPUT_DIR="$1"
+if test -z "$INPUT_DIR" || ! test -s "$INPUT_DIR"; then
+  echo "Please input as first argument the input directory"
+  exit 1
+fi
+
+INPUT_FILE="$2"
+if test -z "$INPUT_FILE" || ! test -s "$INPUT_FILE"; then
+  echo "Please input as second argument the csv input file"
+  exit 1
+fi
+
+OUTPUT_FILE="$3"
+if test -z "$OUTPUT_FILE" || ! test -d "$OUTPUT_FILE"; then
+  echo "Please input as third argument the name of the csv file in which to write the report"
+  exit 1
+fi
+
+
+minet extract -i $INPUT_DIR $INPUT_FILE -o $OUTPUT_FILE
--- event-detection/fetch.sh
+++ event-detection/fetch.sh
@@ -1,3 +1,23 @@
 #!/bin/sh
 
-minet fetch url unique_and_counted_urls.csv --throttle 0 --domain-parallelism 4 --total 1131867 --compress --output-dir FETCHED --folder-strategy prefix-4 -o FETCHED.CSV --resume
+
+INPUT_FILE="$1"
+if test -z "$INPUT_FILE" || ! test -s "$INPUT_FILE"; then
+  echo "Please input as first argument the csv input file"
+  exit 1
+fi
+
+OUTPUT_DIR="$2"
+if test -z "$OUTPUT_DIR" || ! test -d "$OUTPUT_DIR"; then
+  echo "Please input as second argument the path of a directory where to store reports and data"
+  exit 1
+fi
+
+OUTPUT_FILE="$3"
+if test -z "$OUTPUT_FILE" || ! test -d "$OUTPUT_FILE"; then
+  echo "Please input as third argument the name of the csv file in which to write the report"
+  exit 1
+fi
+
+minet fetch url $INPUT_FILE --throttle 0 --domain-parallelism 4 --total 1131867 --compress --output-dir $OUTPUT_DIR --folder-strategy prefix-4 -o $OUTPUT_FILE --resume
+
