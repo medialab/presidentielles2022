062619774f878c127334bb1fdd0b11def05f31d0
bmaz <beatrice.mazoyer@gmail.com>
2023-02-15 18:16:09 +0100

    Add new tweet text rules

--- event-detection/format_documents_depending_on_hostname_prefer_media.py
+++ event-detection/format_documents_depending_on_hostname_prefer_media.py
@@ -27,7 +27,7 @@ from ural import get_domain_name
 
 
 csv.field_size_limit(sys.maxsize)
-TOTAL_TWEETS = 1187326
+TOTAL_TWEETS = 7015695
 
 medias = pd.read_csv('https://raw.githubusercontent.com/medialab/corpora/master/polarisation/medias.csv')
 categories_sorted = ['Mainstream Media', 'Opinion Journalism', 'Counter-Informational Space', 'Periphery', None]
@@ -43,14 +43,26 @@ for unwanted_domain in [None, 'facebook.com','twitter.com', 'youtube.com', 'dail
     media_domains.pop(unwanted_domain)
 
 
-def is_live_stream(url, domain):
+def choose_tweet_text(url, domain, title, description):
     
     return domain == 'bfmtv.com' and 'en-direct' in url \
+    or domain == "francebleu.fr" and "en-direct" in url \
     or domain == 'lemonde.fr' and '/live/' in url \
     or domain == 'ouest-france.fr' and 'direct-presidentielle' in url \
     or domain == 'tf1info.fr' and 'direct' in url \
     or domain == 'dna.fr' and '/elections/' in url \
-    or domain == 'leparisien.fr' and 'les-infos-a-retenir' in url
+    or domain == 'leparisien.fr' and 'les-infos-a-retenir' in url \
+    or domain == 'spotify.com' \
+    or url == 'https://www.tf1info.fr/' \
+    or url == 'https://www.mesopinions.com/fr' \
+    or url == 'https://odysee.com' \
+    or url == 'https://www.challenges.fr/' \
+    or domain == 'amzn.to' \
+    or domain == 'amazon.fr' \
+    or domain == 'lanouvellerepublique.fr' \
+    or domain == '7sur7.be' \
+    or description[:51] == "Profitez des vidéos et de la musique que vous aimez" \
+    or title == "Bloomberg"
 
 
 def one_url_per_tweet_preferably_mainstream_media(source_file, output_file):
@@ -61,13 +73,17 @@ def one_url_per_tweet_preferably_mainstream_media(source_file, output_file):
 
     tweets = {}
     with open(source_file, 'r') as f:
-        reader = casanova.reader(f)
+        reader = casanova.reader(csv.reader(l.replace('\0', '') for l in f))
         
         id_pos = reader.headers.id
-        url_pos = reader.headers.url
+        links_pos = reader.headers.links
+        url_pos = reader.headers.canonical_url
         
         for row in tqdm(reader, total=TOTAL_TWEETS):
-            url = row[url_pos]
+            if row[url_pos]:
+                url = row[url_pos]
+            else:
+                url = row[links_pos]
             if row[id_pos] not in tweets:
                 tweets[row[id_pos]] = url
             else:
@@ -86,27 +102,69 @@ def one_url_per_tweet_preferably_mainstream_media(source_file, output_file):
 
 def write_formated_dataset(source_file, urls_file, output_file):
     tweets = one_url_per_tweet_preferably_mainstream_media(source_file, urls_file)
-
+    added_fields = ["tweet_text", "page_title", "page_description", "page_date", "selected_url", "selected_domain"]
     with open(source_file, 'r') as f,\
     open(output_file, 'w') as of:
-        enricher = casanova.enricher(f, of, keep=['id', 'timestamp', 'text'])
+        enricher = casanova.enricher(csv.reader(l.replace('\0', '') for l in f), of, keep=[
+            "id", 
+            "timestamp_utc", 
+            "user_id",
+            "links",
+            "domains",
+            "text"
+        ], add=added_fields)
         
         id_pos = enricher.headers.id
-        url_pos = enricher.headers.url
+        links_pos = enricher.headers.links
+        url_pos = enricher.headers.canonical_url
+            
         title_pos = enricher.headers.title
         description_pos = enricher.headers.description
-        raw_pos = enricher.headers.raw_content
-        tweet_text_pos = enricher.headers.text
-        for row in tqdm(enricher, total=TOTAL_TWEETS):
-            url = row[url_pos]
-            if tweets[row[id_pos]] == url:
-                domain = get_domain_name(url)
-                    
-                if not is_live_stream(url, domain):
-                    row[tweet_text_pos] = row[title_pos] + ' ' + row[description_pos]
-                    
-                enricher.writerow(row)
+        date_pos = enricher.headers.date
+        
+        text_pos = enricher.headers.text
+        
+        written = set()
 
+        for row in tqdm(enricher, total=TOTAL_TWEETS):
+            if row[url_pos]:
+                url = row[url_pos]
+            else:
+                url = row[links_pos]
+                        
+            if url:
+                if tweets[row[id_pos]] == url:
+                    if row[id_pos] in written:
+                        continue
+                    domain = get_domain_name(url)
+                    
+                    add_list = [
+                        # tweet_text
+                        row[text_pos],
+                        # page_title
+                        row[title_pos],
+                        # page_description
+                        row[description_pos],
+                        # page_date
+                        row[date_pos],
+                        # selected_url
+                        url,
+                        # selected_domain
+                        domain
+                    ]
+                    
+                    if (row[title_pos] or row[description_pos]) and not choose_tweet_text(url, domain, row[text_pos], row[description_pos]):
+                        row[text_pos] = row[title_pos] + ". " + row[description_pos]
+
+                    enricher.writerow(row, add=add_list)
+                    written.add(row[id_pos])
+
+            else:
+                add_list = [
+                    # tweet_text
+                    row[text_pos]
+                ] + ["" for i in range(len(added_fields) - 1)]
+                enricher.writerow(row, add=add_list)
 
 if __name__ == '__main__':
     tweets_file = sys.argv[1]
