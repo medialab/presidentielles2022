6dd43123026f092ee883021f664a5227453fdc01
bmaz <beatrice.mazoyer@gmail.com>
2024-08-21 16:34:13 +0200

    Identify keywords in filenames

--- event-detection/from_event_detection_to_event_files.sh
+++ event-detection/from_event_detection_to_event_files.sh
@@ -8,7 +8,11 @@ WINDOW_SIZE=$3
 STOPWORDS_FILE="stopwords_media_twitter.csv"
 #KEYWORDS="soline|méga-bassine|mégabassine|megabassine|ecoterroris|écoterroris|eco-terroris|éco-terroris"
 #KEYWORDS="lola"
-KEYWORDS="refus d'obtempérer|émeutes|cagnotte|l'haÿ-les-roses|lhaylesroses|violences policières|nanterre|zyed|bouna|clichy-sous-bois|lola|naël|nahel"
+#KEYWORDS="refus d'obtempérer|émeutes|cagnotte|l'haÿ-les-roses|lhaylesroses|violences policières|nanterre|zyed|bouna|clichy-sous-bois|lola|naël|nahel"
+KEYWORDS="jeune conducteur|affrontements|refus d'obtempérer|émeutes|émeutiers|cagnotte|l'haÿ-les-roses|lhaylesroses|violences|nanterre|clichy-sous-bois|naël|nahel"
+
+SAFE_KEYWORDS=$(echo $KEYWORDS | sed -r "s/^(.{30}).*$/\1/" | sed -r "s/[^a-z]/_/gi")
+
 BASE_NAME=$(basename ${INPUT_FILE} | sed "s/\..*//")
 
 eval "$(pyenv init -)"
@@ -37,18 +41,23 @@ echo "5. Find thread ids associated to keywords"
 xsv map "lower(text)" lowered_text ${BASE_NAME}_nn_${THRESHOLD}_${WINDOW_SIZE}_with_text.csv | \
 xsv search -s lowered_text "$KEYWORDS" | \
 xsv frequency -s thread_id -l 0 \
-> ${BASE_NAME}_${THRESHOLD}_${WINDOW_SIZE}_keywords_threads.csv
+> ${BASE_NAME}_${THRESHOLD}_${WINDOW_SIZE}_${SAFE_KEYWORDS}_threads.csv
 
 echo "6. Filter events stats on keywords threads"
-xsv join --left value ${BASE_NAME}_${THRESHOLD}_${WINDOW_SIZE}_keywords_threads.csv thread_id ${BASE_NAME}_${THRESHOLD}_${WINDOW_SIZE}_events_stats.csv |\
-xsv select count,thread_id,nb_docs,top_chi_square_words,top_chi_square_hashtags,top_hashtags,media_urls,tweets_by_media,start_date,end_date,max_docs_date,MPs,tweet_most_RTed,user_most_RTed,id_most_RTed,tweet_percentile,user_percentile,id_percentile \
-> ${BASE_NAME}_${THRESHOLD}_${WINDOW_SIZE}_keywords_events_stats.csv
+xsv join --left value ${BASE_NAME}_${THRESHOLD}_${WINDOW_SIZE}_${SAFE_KEYWORDS}_threads.csv thread_id ${BASE_NAME}_${THRESHOLD}_${WINDOW_SIZE}_events_stats.csv |\
+xsv select count,thread_id,nb_docs,top_chi_square_words,top_chi_square_hashtags,top_hashtags,media_urls,tweets_by_media,start_date,end_date,max_docs_date,MPs \
+> ${BASE_NAME}_${THRESHOLD}_${WINDOW_SIZE}_${SAFE_KEYWORDS}_events_stats.csv
 
-echo "7. Filter on 10 tweets by cluster"
-xsv filter "gte(count, 10)" ${BASE_NAME}_${THRESHOLD}_${WINDOW_SIZE}_keywords_events_stats.csv \
-> ${BASE_NAME}_${THRESHOLD}_${WINDOW_SIZE}_keywords_events_stats_gt10.csv
+echo "7. Filter on 20 tweets by cluster"
+xsv filter "gte(count, 20)" ${BASE_NAME}_${THRESHOLD}_${WINDOW_SIZE}_${SAFE_KEYWORDS}_events_stats.csv \
+> ${BASE_NAME}_${THRESHOLD}_${WINDOW_SIZE}_${SAFE_KEYWORDS}_events_stats_gt20.csv
 
-echo "8. Write one file per thread for threads greater than 10"
-xsv join --left thread_id ${BASE_NAME}_${THRESHOLD}_${WINDOW_SIZE}_keywords_events_stats_gt10.csv thread_id ${BASE_NAME}_nn_${THRESHOLD}_${WINDOW_SIZE}_with_text.csv |\
+echo "8. Write all tweets"
+xsv join --left value ${BASE_NAME}_${THRESHOLD}_${WINDOW_SIZE}_${SAFE_KEYWORDS}_threads.csv thread_id ${BASE_NAME}_nn_${THRESHOLD}_${WINDOW_SIZE}_with_text.csv |\
+xsv select value,count,user_screen_name,retweet_count,text,tweet_text,page_title,page_description,page_date,selected_url,id,nearest_neighbor,user_id,timestamp_utc,thread_id \
+> ${BASE_NAME}_${THRESHOLD}_${WINDOW_SIZE}_original_tweets_from_events_containing_${SAFE_KEYWORDS}.csv
+
+echo "9. Write one file per thread for threads greater than 20"
+xsv join --left thread_id ${BASE_NAME}_${THRESHOLD}_${WINDOW_SIZE}_${SAFE_KEYWORDS}_events_stats_gt20.csv thread_id ${BASE_NAME}_nn_${THRESHOLD}_${WINDOW_SIZE}_with_text.csv |\
 xsv select local_time,user_screen_name,retweet_count,text,tweet_text,page_title,page_description,page_date,selected_url,id,nearest_neighbor,user_id,timestamp_utc,thread_id |\
 xsv partition thread_id ${BASE_NAME}_${THRESHOLD}_${WINDOW_SIZE}_files
